a5eab9279231020053e3cd02457a941c79be3d1f
Patrick Cable II <pc@pcable.net>
2012-11-02 01:48:33 +0000

    Fixed push() syntax and SELECT query

--- checkauthkeys.pl
+++ checkauthkeys.pl
@@ -14,12 +14,12 @@ $connectionInfo = "dbi:mysql:$db;$host";
 $dbh = DBI->connect($connectionInfo,$userid,$passwd);
 
 # Get list of allowed users
-$query = "SELECT * FROM authusers WHERE username='$USER'";
+$query = "SELECT authusers FROM authusers WHERE username='$USER'";
 $runq = $dbh->prepare($query);
 $runq->execute();
 $runq->bind_columns(\$allowed_username);
 while($runq->fetch()) {
-        push @allowed_username_keys $allowed_username;
+        push(@allowed_usernames,$allowed_username);
 }
 
 foreach (@allowed_username_keys) {
