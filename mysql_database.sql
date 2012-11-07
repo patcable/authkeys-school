4b94d0a645c87580e91a9536ebc81c8ded34ee75
Patrick Cable <pc@pcable.net>
2012-11-07 02:59:49 +0000

    Updated database schema

--- mysql_database.sql
+++ mysql_database.sql
@@ -22,8 +22,10 @@ CREATE TABLE IF NOT EXISTS `authkeys_hashes` (
 
 CREATE TABLE IF NOT EXISTS `authusers` (
   `username` varchar(32) NOT NULL,
-  `authusers` varchar(32) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `authusers` varchar(32) NOT NULL,
+  KEY `authusers` (`authusers`),
+  KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -33,6 +35,18 @@ CREATE TABLE IF NOT EXISTS `authusers` (
 
 CREATE TABLE IF NOT EXISTS `usercerts` (
   `user` varchar(32) NOT NULL,
-  `cert` text NOT NULL
+  `cert` text NOT NULL,
+  UNIQUE KEY `user_1` (`user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `authusers`
+--
+ALTER TABLE `authusers`
+  ADD CONSTRAINT `authusers_ibfk_1` FOREIGN KEY (`authusers`) REFERENCES `usercerts` (`user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `authusers_ibfk_2` FOREIGN KEY (`username`) REFERENCES `usercerts` (`user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
